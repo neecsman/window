3509aae3c3ad076a6fc17d756f433cbb2817d56a
Nikita <nikappleid@yandex.ru>
2022-03-06 15:38:31 +0300

    Classic Project - Window

---
+++ src/js/modules/timer.js
@@ -0,0 +1,62 @@
+const timer = (id, deadline) => {
+
+    const addZero = (num) => {
+        if (num <= 9) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }; 
+
+    const getTimeRemaning = (endtime) => {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              seconds = Math.floor((t/1000) % 60),
+              minutes = Math.floor((t/1000/60) % 60),
+              hours = Math.floor((t/(1000 * 60 * 60)) % 24),
+              days = Math.floor((t/(1000 * 60 * 60 * 24)));
+
+    
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours,
+            'days': days
+        };
+    };
+
+    const setClock = (selector, endtime) => {
+        const timer = document.querySelector(selector),
+              days = document.querySelector('#days'),
+              hours = document.querySelector('#hours'),
+              minutes = document.querySelector('#minutes'),
+              seconds = document.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+        
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+            
+            if(t.total <= 0) {
+                days.textContent = "00";
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+
+                clearImmediate(timeInterval);
+             
+            }
+        }
+    };
+
+    setClock(id, deadline);
+
+};
+
+export default timer;
